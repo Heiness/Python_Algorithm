5704c058fbe6e2326d67ea8f5fdd8af455b04165
정현호 <103182268+Heiness@users.noreply.github.com>
2023-06-28 00:07:57 +0900

    Create 9095_123더하기.py

---
+++ Baekjoon/DP/9095_123더하기.py
@@ -0,0 +1,16 @@
+import sys; input=sys.stdin.readline
+
+for _ in range(int(input())):
+    n = int(input())
+    d = [0] * (n+1)
+    if n==1: print(1); continue
+    elif n==2: print(2); continue
+    elif n==3: print(4); continue
+    d[1] = 1
+    d[2] = 2 
+    d[3] = 4 
+
+    for i in range(4,n+1):
+        d[i] = d[i-1] + d[i-2] + d[i-3]
+
+    print(d[-1])
