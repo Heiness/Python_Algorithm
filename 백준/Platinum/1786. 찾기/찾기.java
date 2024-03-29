0543f5b05d7533f0532dd4c495b48f8c35b73bf4
정현호 <103182268+Heiness@users.noreply.github.com>
2024-03-29 10:06:21 +0900

    [Platinum V] Title: 찾기, Time: 2948 ms, Memory: 96580 KB -BaekjoonHub

---
+++ 백준/Platinum/1786. 찾기/찾기.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+import javax.security.auth.login.AccountLockedException;
+
+public class Main {
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String T = br.readLine();
+		String P = br.readLine();
+		
+		KMP(T,P);
+	}
+	
+	static void KMP(String T, String P) {
+		// 파이 배열 구하기
+		int[] piArr = new int[P.length()];
+		int i = 0;
+		for(int j=1;j<P.length();j++) {
+			while(i>0 && P.charAt(i)!=P.charAt(j)) i = piArr[i-1];
+			if(P.charAt(i)==P.charAt(j)) piArr[j]=++i;
+		}
+		
+		// 패턴 존재 여부 확인
+		List<Integer> result = new ArrayList<>();
+		i = 0;
+		for(int j=0;j<T.length();j++) {
+			while(i>0 && P.charAt(i) != T.charAt(j)) i = piArr[i-1];
+			if(P.charAt(i)==T.charAt(j)) {
+				if(++i==P.length()) {
+					result.add(j-i+2);
+					i = piArr[i-1];
+				}
+			}
+		}
+		
+		System.out.println(result.size());
+		for(int n: result) System.out.print(n+" ");
+	}
+}
