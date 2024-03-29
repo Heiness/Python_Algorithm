9ee18c97fd56b7cb7c31b3d728b72c4b002736dd
정현호 <103182268+Heiness@users.noreply.github.com>
2024-03-29 11:24:40 +0900

    [Gold II] Title: 벽 부수고 이동하기 4, Time: 1100 ms, Memory: 43904 KB -BaekjoonHub

--- 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.py
+++ 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.py
@@ -1,53 +1,39 @@
-from collections import deque
-input = __import__('sys').stdin.readline
-
-def bfs(start):
-    q = deque()
-    q.append(start)
-    cnt = 1
-    while q:
-        i, j = q.popleft()
-        zeros[i][j] = group
-        for idx in range(4):
-            ni, nj = i + dy[idx], j + dx[idx]
-            if ni < 0 or ni >= n or nj < 0 or nj >= m or visited[ni][nj] or graph[ni][nj] == 1:
-                continue
-            visited[ni][nj] = True
-            q.append((ni, nj))
-            cnt += 1
-    return cnt
-
-n, m = map(int, input().split())
-graph = [list(map(int, input().strip())) for _ in range(n)]
-visited = [[False] * m for _ in range(n)]
-zeros = [[0] * m for _ in range(n)]
-group = 1
-info = {}
-info[0] = 0
-
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
-
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 0 and not visited[i][j]:
-            visited[i][j] = True
-            w = bfs((i, j))
-            info[group] = w
-            group += 1
-
-for i in range(n):
-    for j in range(m):
-        addList = set()
-        if graph[i][j]:
-            for idx in range(4):
-                ni, nj = i + dy[idx], j + dx[idx]
-                if ni < 0 or ni >= n or nj < 0 or nj >= m:
-                    continue
-                addList.add(zeros[ni][nj])
-            for add in addList:
-                graph[i][j] += info[add]
-                graph[i][j] %= 10
-
-for g in graph:
-    print("".join(map(str, g)))
+import sys
+def main():
+    input = sys.stdin.readline
+    N, M = map(int, input().split())
+    d = ((-1, 0), (1, 0), (0, -1), (0, 1))
+    graph = [list(input().rstrip()) for _ in range(N)]
+    groups, group = [0,0], 2
+    def count_group(q, num):
+        count = 1
+        while q:
+            r, c = q.pop()
+            for dr, dc in d:
+                if 0 <= (nr := r + dr) < N and 0 <= (nc := c + dc) < M:
+                    if graph[nr][nc] == "0":
+                        count += 1; graph[nr][nc] = num
+                        q.append((nr, nc))
+        return count % 10
+    for r in range(N):
+        for c in range(M):
+            if graph[r][c] == "0":
+                q = [(r, c)]
+                graph[r][c] = group
+                groups.append(count_group(q, group))
+                group += 1
+    for r in range(N):
+        ans = []
+        for c in range(M):
+            if graph[r][c] == "1":
+                count, tmp = 1, set()
+                for dr, dc in d:
+                    if 0 <= (nr := r + dr) < N and 0 <= (nc := c + dc) < M:
+                        if (t := graph[nr][nc]) != "1": tmp.add(t)
+                for n in tmp: count += groups[n]
+                ans.append(count % 10)
+            else: ans.append("0")
+        print("".join(map(str, ans)))
+        
+if __name__ == "__main__":
+    main()
