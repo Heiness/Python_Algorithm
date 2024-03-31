5e4e64a1a5f90a832cc78b371041eea577f40005
정현호 <103182268+Heiness@users.noreply.github.com>
2024-03-31 20:30:54 +0900

    [Gold IV] Title: 여행 가자, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -1,35 +1,27 @@
-import sys
-input = sys.stdin.readline
+import sys; input = sys.stdin.readline
 
-def union(x,y):
-    x = find(x)
-    y = find(y)
-    if x < y:
-        parents[y] = x
-    else:
-        parents[x] = y
-    
 def find(x):
-    if x != parents[x]:
-        parents[x] = find(parents[x])
-    return parents[x]
+    if parent[x]!=x: parent[x] = find(parent[x])
+    return parent[x]
 
-# 유니온 파인드
-n,m = int(input()),int(input())
-parents = [i for i in range(n)]
-for i in range(n):
-    link = list(map(int,input().split()))
-    for j in range(n):
-        if link[j] == 1:
-            union(i,j)
+def union(a,b):
+    a,b = find(a), find(b)
+    if a<b: parent[b]=a
+    else: parent[a]=b
 
-# 경로 체크
-parents = [-1] + parents
-path = list(map(int,input().split()))
-start = parents[path[0]]
-for i in range(1,m):
-    if parents[path[i]] != start:
-        print("NO")
-        break
-else:
-    print("YES")
+
+N, M = int(input()), int(input())
+parent = list(range(N+1))
+
+for i in range(N):
+    t = list(map(int,input().split()))
+    for idx,v in enumerate(t):
+        if i==idx: continue
+        if v==1: union(i+1,idx+1)
+
+l = list(map(int,input().split()))
+for i in range(len(l)-1):
+    if find(l[i])==find(l[i+1]): continue
+    print("NO")
+    exit()
+print("YES")
