4d1b05348dd5f97c7a10735cb50cb6e6164df7a9
정현호 <103182268+Heiness@users.noreply.github.com>
2024-04-03 09:48:12 +0900

    [Gold III] Title: 전구, Time: 52 ms, Memory: 34528 KB -BaekjoonHub

--- 백준/Gold/2550. 전구/전구.py
+++ 백준/Gold/2550. 전구/전구.py
@@ -1,24 +1,28 @@
 import sys; input = sys.stdin.readline
 from bisect import bisect_left
 N = int(input())
-s, l = list(map(int,input().split())), list(map(int,input().split()))
-idxs = [l.index(v) for v in s]
-res = [idxs[0]]
-cnt = [1]
-for v in idxs[1:]:
-    if res[-1]<v: 
+switch = list(map(int,input().split()))
+idxSwitch = [0] * (N+1)
+for i,v in enumerate(switch): idxSwitch[v] = i
+light = list(map(int,input().split()))
+nums = [ idxSwitch[v] for v in light ]
+
+res = [nums[0]]; cnt=[1]
+l = 1
+for v in nums[1:]:
+    if res[-1]<v:
         res.append(v)
-        cnt.append(len(res))
+        l+=1
+        cnt.append(l)
     else:
         idx = bisect_left(res,v)
         res[idx] = v
         cnt.append(idx+1)
-
-tmp = len(res)
-print(tmp)
+        
+print(l)
 ans = []
 for i in range(N-1,-1,-1):
-    if tmp==cnt[i]:
-        ans.append(s[i])
-        tmp-=1
+    if l==cnt[i]:
+        ans.append(light[i])
+        l-=1
 print(*sorted(ans))
