5c01c7290176d557a8d9f6734c9d59357b214ae5
정현호 <103182268+Heiness@users.noreply.github.com>
2025-01-25 14:46:08 +0900

    [Silver II] Title: 이동하기, Time: 2076 ms, Memory: 80568 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -0,0 +1,20 @@
+import sys; input = sys.stdin.readline
+
+N, M = map(int,input().split())
+graph = [ list(map(int,input().split())) for _ in range(N) ]
+dp = [[0] * M for _ in range(N)] 
+dp[0][0]=graph[0][0]
+
+dy = [0,1,1]
+dx = [1,0,1]
+
+for i in range(N):
+    for j in range(M):
+        for k in range(3):
+            ny = i + dy[k]
+            nx = j + dx[k]
+
+            if 0<=ny<N and 0<=nx<M:
+                dp[ny][nx] = max(dp[ny][nx], dp[i][j]+graph[ny][nx])
+
+print(dp[-1][-1])
