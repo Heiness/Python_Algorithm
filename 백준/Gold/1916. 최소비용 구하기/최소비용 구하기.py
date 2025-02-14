72bf18845e8c3143924af652bdc98f15440961b7
정현호 <103182268+Heiness@users.noreply.github.com>
2025-02-14 10:00:13 +0900

    [Gold V] Title: 최소비용 구하기, Time: 240 ms, Memory: 65160 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -0,0 +1,36 @@
+import sys; input = sys.stdin.readline
+import heapq
+
+N = int(input())
+M = int(input())
+graph = [[] for _ in range(N+1)]
+distance = [sys.maxsize] * (N+1)
+
+for _ in range(M):
+    a, b, c = map(int,input().split())
+    graph[a].append((b,c))
+    graph[b].append((b,c))
+
+start, end = map(int,input().split())
+
+def dijk(n: int):
+    global end
+    pq = []
+    heapq.heappush(pq,(0, n))
+    distance[n] = 0
+
+    while pq:
+        c, now = heapq.heappop(pq)
+
+        if distance[now]<c: continue
+        if now == end: return
+
+        for next, cost in graph[now]:
+            nc = c + cost
+
+            if distance[next] > nc:
+                distance[next] = nc
+                heapq.heappush(pq,(nc,next))
+
+dijk(start)
+print(distance[end])
