c295fc3142b24ad2536358a14012424117a1641b
정현호 <103182268+Heiness@users.noreply.github.com>
2025-02-19 20:18:11 +0900

    [Gold IV] Title: 거짓말, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.py
+++ 백준/Gold/1043. 거짓말/거짓말.py
@@ -1,15 +1,22 @@
-import sys; input = sys.stdin.readline
+import sys
+input = sys.stdin.readline
 
-N, M = map(int,input().split())
-knownList = set(list(map(int,input().split()))[1:])
+n, m = map(int, input().split())
+knowList = set(input().split()[1:])
 parties = []
-for _ in range(M): parties.append(set(list(map(int,input().split()))[1:]))
-for _ in range(M):
-    for party in parties: 
-        if knownList & party: knownList = knownList.union(party)
 
-ans = 0
+for _ in range(m):
+    parties.append(set(input().split()[1:]))
+
+for _ in range(m):
+    for party in parties:
+        if party & knowList:
+            knowList = knowList.union(party)
+
+cnt = 0
 for party in parties:
-    if knownList & party: continue
-    ans+=1
-print(ans)
+    if party & knowList:
+        continue
+    cnt += 1
+
+print(cnt)
