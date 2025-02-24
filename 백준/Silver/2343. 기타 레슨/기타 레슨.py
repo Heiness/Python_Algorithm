e8c49cf15e538585e923f2c0769aca4fc071b6bd
정현호 <103182268+Heiness@users.noreply.github.com>
2025-02-24 19:38:49 +0900

    [Silver I] Title: 기타 레슨, Time: 596 ms, Memory: 42168 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.py
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.py
@@ -1,29 +1,23 @@
 import sys; input = sys.stdin.readline
 
-N, M = map(int, input().split())
-nums = list(map(int, input().split()))
+N, M = map(int,input().split())
+nums = list(map(int,input().split()))
 start = max(nums)
 end = sum(nums)
-ans = sys.maxsize
+
 
 while start<=end:
     mid = (start+end)//2
-    total = 0
-    cnt = 1
-    for num in nums:
-        if total + num > mid:
-            # print(f"{num}, {total+num}, {mid}") # 1 2 3 4 5 / 6 7 / 
-            cnt+=1
-            total = 0
-            # continue
-        total += num
-    # print(ans, cnt, mid)
-    
-    if cnt <= M:
-        # print(f"갱신 {ans}, {mid}, {M}, {cnt}")
-        ans = min(ans, mid)
-        end = mid -1
-    else:
-        start = mid + 1
-    
-print(ans)
+
+    count=1
+    tmp=0
+
+    for v in nums:
+        if tmp + v > mid:
+            count += 1
+            tmp = 0
+        tmp+=v
+    if count <= M: end=mid-1
+    else: start=mid+1
+
+print(start)
