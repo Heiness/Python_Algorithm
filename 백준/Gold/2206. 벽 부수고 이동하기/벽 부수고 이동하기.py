d47796c525280f766641c0fca9e99a8fa4708dff
정현호 <103182268+Heiness@users.noreply.github.com>
2025-10-17 23:38:25 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 6228 ms, Memory: 191736 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,21 +1,21 @@
 import sys; input = sys.stdin.readline
 from collections import deque
 
-N, M = map(int,input().split())
-graph = [ list(map(int,input().strip())) for _ in range(N) ]
-visited = [[[0,0] for _ in range(M)] for i in range(N)]
-visited[0][0][0] = 1
+N, M = map(int, input().split())
+arr = [ list(map(int,input().strip())) for _ in range(N) ]
+visited = [ [[0] * 2 for _ in range(M)] for __ in range(N) ]
 
 dy = [0,0,-1,1]
 dx = [-1,1,0,0]
 
 q = deque()
 q.append((0,0,0))
+visited[0][0][0] = 1
 
 while q:
     y, x, c = q.popleft()
-    
-    if y == N-1 and x == M-1:
+
+    if y==N-1 and x==M-1:
         print(visited[y][x][c])
         break
 
@@ -23,16 +23,15 @@ while q:
         ny = y + dy[i]
         nx = x + dx[i]
 
-        if ny<0 or N<=ny or nx<0 or M<=nx:
-            continue
+        if 0<=ny<N and 0<=nx<M:
+            # 벽이 있지만 안부셨을때
+            if arr[ny][nx] == 1 and not c and not visited[ny][nx][1]:
+                visited[ny][nx][1] = visited[y][x][c] + 1
+                q.append((ny,nx,c+1))
 
-        if graph[ny][nx]==1 and not c:
-            visited[ny][nx][1] = visited[y][x][0] + 1
-            q.append((ny,nx,1))
-        
-        elif graph[ny][nx]==0 and visited[ny][nx][c] == 0:
-            visited[ny][nx][c] = visited[y][x][c] + 1
-            q.append((ny,nx,c))
+            # 벽이 없을 때
+            elif arr[ny][nx] == 0 and not visited[ny][nx][c]:
+                visited[ny][nx][c] = visited[y][x][c] + 1
+                q.append((ny,nx,c))
 
-else:
-    print(-1)
+else: print(-1)
